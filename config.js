6a764e86265226cea69b8b691cab0b6712136fe7
abdullahqadeer4 <abdullahqadeer420@gmail.com>
2025-02-01 11:12:08 +0500

    changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let config = {
   emailjs: {
     serviceID: "gmail",
     templateID: "template_zHretJ0d",
-    userID: "user_8g0rh3d6Qj1QZBEU8USls"
+    userID: "abdullahqadeer420@gmai.com"
   }
 };
 
--- info.js
+++ info.js
@@ -14,7 +14,7 @@ let info = {
     linkedin: "https://www.linkedin.com/in/abdullah-qadeer-9485611b5?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bg2FT5Il2RXaeJaOmQOPGjQ%3D%3D",
     github: "https://github.com/Abdullahqadeer4",
     hackerRank: "https://www.hackerrank.com/profile/abdullahqadeer41",
-    resume: "https://drive.google.com/file/d/1W0pzOo1Gb-W70BYKehOfZ4qaJt7m1_7O/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1Q7lfsS0uhO3zdQsR_tcJSlQ6JNoIMfrP/view?usp=sharing",
   },
   education: [
     {
@@ -79,268 +79,244 @@ let info = {
       icon: "fas fa-laptop-code"
     },
   ],
-  portfolio_design: [
-    {
-      name: "Keep it Green",
-      title: "UI/UX Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/Keep it green/cover.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/Keep it green/show.png"),
-          title: "show"
-        },
-      ],
-      technologies: ["Figma", "Photoshop"],
-      category: "Visual Design",
-      github: "",
-      date: "Jan, 2022",
-      visit: "",
-      description:
-        ""
-    },
-    {
-      name: "Digital agency website",
-      title: "UI/UX Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/digital agency/behance cover.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/digital agency/1. start.png"),
-          title: "App Elements"
-        },
-        {
-          img: require("./src/assets/designs/digital agency/2. about.png"),
-          title: "App Elements"
-        },
-        {
-          img: require("./src/assets/designs/digital agency/3. Our services.png"),
-          title: "App Elements"
-        },
-        {
-          img: require("./src/assets/designs/digital agency/4. latest project.png"),
-          title: "App Elements"
-        },
-        {
-          img: require("./src/assets/designs/digital agency/5. Contact us.png"),
-          title: "App Elements"
-        },
-      ],
-      technologies: ["Figma", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "Dec, 2021",
-      visit: "",
-      description:
-        ""
-    },
-    {
-      name: "Credit card design",
-      title: "Illustration",
-      pictures: [
-        {
-          img: require("./src/assets/designs/credit/behance cover.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/credit/degisning.png"),
-          title: "Degisning"
-        },
-        {
-          img: require("./src/assets/designs/credit/Dual tune.png"),
-          title: "Dual tune"
-        },
-        {
-          img: require("./src/assets/designs/credit/Glassmorphism.png"),
-          title: "Glassmorphism"
-        },
-        {
-          img: require("./src/assets/designs/credit/Gradiant.png"),
-          title: "Gradiant"
-        },
-        {
-          img: require("./src/assets/designs/credit/Monochrome.png"),
-          title: "Monochrome"
-        },
-        {
-          img: require("./src/assets/designs/credit/Neumorphism.png"),
-          title: "Neumorphism"
-        },
-      ],
-      technologies: ["Figma", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "Dec, 2021",
-      visit: "",
-      description:
-        ""
-    },
-    {
-      name: "BOOKSI",
-      title: "E-Book library - UI/UX Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/booksi/behance cover.png"),
-          title: "Cover"
-        },
-        {
-          img: require("./src/assets/designs/booksi/behance board.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/booksi/login.png"),
-          title: "login"
-        },
-        {
-          img: require("./src/assets/designs/booksi/register.png"),
-          title: "register"
-        },
-        {
-          img: require("./src/assets/designs/booksi/home page.png"),
-          title: "homepage"
-        },
-        {
-          img: require("./src/assets/designs/booksi/For you.png"),
-          title: "just for you"
-        }
-      ],
-      technologies: ["Figma", "Photoshop"],
-      category: "Visual Design",
-      github: "",
-      date: "Nov, 2021",
-      visit: "",
-      description:
-        " BOOKSI is Ebook android app for easy read books."
-    },
-    {
-      name: "Quiet Weather",
-      title: "UI/UX Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/weather/Wooden Hand iPhone 12 Pro.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/weather/artboard.png"),
-          title: "show"
-        },
-        {
-          img: require("./src/assets/designs/weather/weather app react native.png"),
-          title: "components"
-        },
-      ],
-      technologies: ["Figma", "Illustration"],
-      category: "Visual Design",
-      github: "",
-      date: "Nov, 2021",
-      visit: "",
-      description:
-        "Quiet Weather is a react native app by openweathermap api, i designed the ui/ux and coding it in react native, enjoy it!"
-    },
-    {
-      name: "Pill Reminder",
-      title: "Pill Reminder - Mockup Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/pill reminder/Web 1.png"),
-          title: "Moodboard"
-        },
-        {
-          img: require("./src/assets/designs/pill reminder/1.png"),
-          title: "Mockups 1"
-        },
-        {
-          img: require("./src/assets/designs/pill reminder/2.png"),
-          title: "Mockups 2"
-        }
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "Apr, 2021",
-      visit: "",
-      description:
-        "pill reminder app for nessecary drugs for the day."
-    },
-  ],
+  // portfolio_design: [
+  //   {
+  //     name: "Keep it Green",
+  //     title: "UI/UX Design",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/Keep it green/cover.png"),
+  //         title: "MoodBoard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/Keep it green/show.png"),
+  //         title: "show"
+  //       },
+  //     ],
+  //     technologies: ["Figma", "Photoshop"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Jan, 2022",
+  //     visit: "",
+  //     description:
+  //       ""
+  //   },
+  //   {
+  //     name: "Digital agency website",
+  //     title: "UI/UX Design",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/digital agency/behance cover.png"),
+  //         title: "MoodBoard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/digital agency/1. start.png"),
+  //         title: "App Elements"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/digital agency/2. about.png"),
+  //         title: "App Elements"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/digital agency/3. Our services.png"),
+  //         title: "App Elements"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/digital agency/4. latest project.png"),
+  //         title: "App Elements"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/digital agency/5. Contact us.png"),
+  //         title: "App Elements"
+  //       },
+  //     ],
+  //     technologies: ["Figma", "Illustrator"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Dec, 2021",
+  //     visit: "",
+  //     description:
+  //       ""
+  //   },
+  //   {
+  //     name: "Credit card design",
+  //     title: "Illustration",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/credit/behance cover.png"),
+  //         title: "MoodBoard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/degisning.png"),
+  //         title: "Degisning"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/Dual tune.png"),
+  //         title: "Dual tune"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/Glassmorphism.png"),
+  //         title: "Glassmorphism"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/Gradiant.png"),
+  //         title: "Gradiant"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/Monochrome.png"),
+  //         title: "Monochrome"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/credit/Neumorphism.png"),
+  //         title: "Neumorphism"
+  //       },
+  //     ],
+  //     technologies: ["Figma", "Illustrator"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Dec, 2021",
+  //     visit: "",
+  //     description:
+  //       ""
+  //   },
+  //   {
+  //     name: "BOOKSI",
+  //     title: "E-Book library - UI/UX Design",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/booksi/behance cover.png"),
+  //         title: "Cover"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/booksi/behance board.png"),
+  //         title: "MoodBoard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/booksi/login.png"),
+  //         title: "login"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/booksi/register.png"),
+  //         title: "register"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/booksi/home page.png"),
+  //         title: "homepage"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/booksi/For you.png"),
+  //         title: "just for you"
+  //       }
+  //     ],
+  //     technologies: ["Figma", "Photoshop"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Nov, 2021",
+  //     visit: "",
+  //     description:
+  //       " BOOKSI is Ebook android app for easy read books."
+  //   },
+  //   {
+  //     name: "Quiet Weather",
+  //     title: "UI/UX Design",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/weather/Wooden Hand iPhone 12 Pro.png"),
+  //         title: "MoodBoard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/weather/artboard.png"),
+  //         title: "show"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/weather/weather app react native.png"),
+  //         title: "components"
+  //       },
+  //     ],
+  //     technologies: ["Figma", "Illustration"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Nov, 2021",
+  //     visit: "",
+  //     description:
+  //       "Quiet Weather is a react native app by openweathermap api, i designed the ui/ux and coding it in react native, enjoy it!"
+  //   },
+  //   {
+  //     name: "Pill Reminder",
+  //     title: "Pill Reminder - Mockup Design",
+  //     pictures: [
+  //       {
+  //         img: require("./src/assets/designs/pill reminder/Web 1.png"),
+  //         title: "Moodboard"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/pill reminder/1.png"),
+  //         title: "Mockups 1"
+  //       },
+  //       {
+  //         img: require("./src/assets/designs/pill reminder/2.png"),
+  //         title: "Mockups 2"
+  //       }
+  //     ],
+  //     technologies: ["XD", "Illustrator"],
+  //     category: "Visual Design",
+  //     github: "",
+  //     date: "Apr, 2021",
+  //     visit: "",
+  //     description:
+  //       "pill reminder app for nessecary drugs for the day."
+  //   },
+  // ],
   portfolio: [
     {
-      name: "Diary App",
+      name: "Booking App",
       pictures: [
+        // {
+        //   img: require("./src/assets/portfolio/diary/booking.jpg")
+        // },
         {
-          img: require("./src/assets/portfolio/diary/1.png")
+          img: require("./src/assets/portfolio/diary/2.jpg")
         },
-        {
-          img: require("./src/assets/portfolio/diary/2.png")
-        },
-        {
-          img: require("./src/assets/portfolio/diary/3.png")
-        },
+        // {
+        //   img: require("./src/assets/portfolio/diary/3.png")
+        // },
       ],
-      technologies: ["react", "scss", "local storage"],
+      technologies: ["Vue2", "scss", "Django", "Postgresql", "Background Tasks", "Django Rest Framework"],
       category: "Web App",
       date: "Aug, 2021 - 20 days",
       github:
         "https://github.com/mahy209/notes-react-app.git",
-      visit: "https://github.com/mahy209/notes-react-app.git",
+      visit: "https://afp.gov.pk/",
       description:
-        " Diary react app with some new features: Dark mode, upload images, tags, filter search with title/content/tag "
+        "Booking App for Army Forces Pakistan where they can book rooms in different messes according to their army status"
     },
     {
-      name: "Online Examination",
+      name: "CCLC - Care Channel Learning Center",
       pictures: [
         {
-          img: require("./src/assets/portfolio/online examination/exam (1).png")
+          img: require("./src/assets/portfolio/online examination/cclc.png")
         },
-        {
-          img: require("./src/assets/portfolio/online examination/exam (2).png")
-        },
-        {
-          img: require("./src/assets/portfolio/online examination/exam (3).png")
-        },
-        {
-          img: require("./src/assets/portfolio/online examination/exam (4).png")
-        },
-        {
-          img: require("./src/assets/portfolio/online examination/exam (5).png")
-        }
       ],
-      technologies: ["HTML", "CSS","JavaSript","JQuery", "PHP", "MySQL"],
+      technologies: ["Vue3", "Tailwind","Django","Django Rest Framework", "MySQL"],
       category: "Web App",
-      date: "Apr, 2021 - May, 2021",
-      github: "https://github.com/mahy209/online-examination.git",
-      visit: "https://github.com/mahy209/online-examination.git",
+      date: "In Progress",
+      github: "https://github.com/abdullah131566/cclc-fe",
       description:
-        "Online Examination System Today Online Examination System has become a fast growing examination method because of its speed and accuracy. It is also needed less manpower to execute the examination. Almost all organizations now-a-days, are conducting their objective exams by online examination system, it saves students time in examinations. Organizations can also easily check the performance of the student that they give in an examination. As a result of this, organizations are releasing results in less time. It also helps the environment by saving paper. According to today’s requirement, online examination project in php is very useful to learn it. What is an online examination system? In an online examination system examine get their user id and password with his/her admit card. This id is already saved in the examination server. When examine login to the server he/she get his/her profile already register. On the certain time examine gets the message to start the examination. All answers given by examine are saved into the server with his/her profile information. Online examination system also allows to correct the answer if the examine needed to change any answer in the examination time duration, however, after the time duration any change will not allow. This also makes c checking the answer easy and error proof as computers are more accurate than man and provide fast results too. Php is a web base language so we can create an online examination system in PHP. Administrator of Online Examination has multiple features such as Add, Delete, Update Topics and Question. To Login as Admin put inside your browser."
+        "CCLC is an role based platform for advisors, students, teachers and admin. Where they can interact with each other and can take online exams and quizzes and other features",
+      // visit: "https://github.com/abdullah131566/cclc-fe",
     },
     {
-      name: "MyFlix.com",
+      name: "Ecare",
       pictures: [
         {
-          img: require("./src/assets/portfolio/myflix.com/1.png")
-        },
-        {
-          img: require("./src/assets/portfolio/myflix.com/2.png")
-        },
-        {
-          img: require("./src/assets/portfolio/myflix.com/3.png")
-        },
-        {
-          img: require("./src/assets/portfolio/myflix.com/4.png")
+          img: require("./src/assets/portfolio/myflix.com/ecare.png")
         },
       ],
-      technologies: ["react", "css", "TMDB"],
+      technologies: ["Vue2", "css", "Bootstrap"],
       category: "Web App",
-      date: "Aug, 2021 - 10 days",
-      github:
-        "https://github.com/mahy209/MyFlix-react-app.git",
-      visit: "https://github.com/mahy209/MyFlix-react-app.git",
+      date: "Aug, 2023",
       description:
-        " flix.com is a netflix clone using React JS and TMDB with courusal and search engine. "
+        "Ecare is an online consultation app where doctor and patient can interact with each other"
     },
   ],
 };
